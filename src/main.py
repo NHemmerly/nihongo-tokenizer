db782d86112f165f7a037b63509e64dd0cff37d0
Neil Hemmerly <neilhemm0@gmail.com>
2025-07-16 00:21:46 -0400

    organize output into a list of dicts

--- src/main.py
+++ src/main.py
@@ -1,9 +1,20 @@
 import MeCab
 
 def main():
-    tokenizer = MeCab.Tagger("-Owakati -r /dev/null -d ./lib/python3.9/site-packages/unidic_lite/dicdir")
-    tokens = tokenizer.parse("こんにちは、僕の名前はニールです。日本語を話せます。").split()
-    print(tokens)
+    tokenizer = MeCab.Tagger("-r /dev/null -d ./lib/python3.9/site-packages/unidic_lite/dicdir")
+    tokens = tokenizer.parse("こんにちは、僕の名前はニールです。日本語を話せます。").split("\n")
+    token_dict = []
+    for line in tokens:
+        out = line.split("\t")
+        if len(out) > 1:
+            token = {
+                "surface": out[0],
+                "reading": out[1],
+                "lem": out[3],
+                "pos": out[4]
+            }
+        token_dict.append(token)
+    print(token_dict)
 
 if __name__ == "__main__":
     main()
